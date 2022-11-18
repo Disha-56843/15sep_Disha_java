1e207673ef79004d6776eb18a97157fabe5c6220
Disha-56843 <113577480+Disha-56843@users.noreply.github.com>
2022-11-18 19:31:10 +0530

    Add files via upload

---
+++ Que-2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	a=15;
+	b=12;
+	printf("Addition of a and b is :%d",a+b);
+	printf("\nSubstraction of a and b is :%d",a-b);
+	printf("\nMultiplication of a and b is :%d",a*b);
+	printf("\nDivission of a and b is :%d",a/b);
+	printf("\nModulo of a and b is :%d",a%b);
+}

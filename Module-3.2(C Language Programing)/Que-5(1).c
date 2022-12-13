716bc7e50ddd67bab219e9c63afeffea92e32396
Disha-56843 <dishajasani56843@gmail.com>
2022-12-13 20:43:00 +0530

    Revert "Delete Module-3.2(C Language Programing) directory"
    This reverts commit 95964a76b81874904ef9dcc293262880db64c7c1.

---
+++ Module-3.2(C Language Programing)/Que-5(1).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int no;
+	no = 972;
+	for ( no = 972; no>=897; no--)
+	{
+		printf("\n%d",no);
+	}
+}
---
+++ Module-3.2(C Language Programing)/Que-5(2).c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int no[10];
+	printf("Enter any ten no.:");
+	scanf("%d",no[10]);
+	
+}

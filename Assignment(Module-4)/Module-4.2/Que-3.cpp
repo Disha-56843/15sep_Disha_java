5eb1044ca355884b8e6fd75f0b53fa50356455ec
Disha-56843 <113577480+Disha-56843@users.noreply.github.com>
2023-01-17 12:37:54 +0530

    Add files via upload

---
+++ Assignment(Module-4)/Module-4.2/Que-3.cpp
@@ -0,0 +1,28 @@
+//Write a program to find the multiplication values and the cubic values using inline function.
+#include<iostream>
+using namespace std;
+class Multiplication
+{
+    public:
+    inline int mul (int a, int b)
+    {
+        return a * b ;
+    }
+};
+class Cubic 
+{
+    public:
+    inline int cube ( int a)
+    {
+        return a*a*a;
+    }
+};
+int main ()
+{
+    Multiplication m1;
+    Cubic c1;
+    cout<<"Mutliplication of two values is:"<<m1.mul( 12,23);
+    cout<<endl<<"Cube of two value is:"<<c1.cube(10);
+
+    return 0;
+}
